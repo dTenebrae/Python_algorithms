13c06932ff53cd871e249623f2f46b7e43c3e987
dTenebrae <dtenebrae@gmail.com>
2020-06-11 14:58:37 +0300

    lesson 6 final commit

--- size.py
+++ size.py
@@ -12,5 +12,7 @@ def size_of(x, level=0):
                 size_of(xx, level + 1)
 
 
-print(sys.version, sys.platform)
-
+def info_size(x, name='unknown'):
+    print(f'Имя, тип и размер объекта: {name}, {type(x)}, {sys.getsizeof(x)} байт'
+          f'\nОбъем информации в объекте: {sys.getsizeof(x) - sys.getsizeof(type(x)())} байт'
+          f'\nРазмер пустого объекта: {sys.getsizeof(type(x)())} байт')
